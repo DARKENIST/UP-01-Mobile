321b1f18da1fbc509d2bdf8dd136e8f015f27fe7
DARKENIST <stepanhunter2007@gmail.com>
2026-01-28 16:45:25 +0700

    Пакет data заполнен пакетами с классами

---
+++ app/src/main/java/com/example/natkcollegeschedule/data/api/ScheduleApi.kt
@@ -0,0 +1,16 @@
+package com.example.natkcollegeschedule.data.api
+
+import com.example.natkcollegeschedule.data.dto.ScheduleByDateDto
+import retrofit2.http.GET
+import retrofit2.http.Path
+import retrofit2.http.Query
+
+interface ScheduleApi {
+
+    @GET("api/schedule/group/{groupName}")
+    suspend fun getSchedule(
+        @Path("groupName") groupName: String,
+        @Query("start") start: String,
+        @Query("end") end: String
+    ): List<ScheduleByDateDto>
+}
